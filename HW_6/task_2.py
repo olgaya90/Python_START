d94ecd62f4927aa94c3d0dbdfd1e061cb1f381af
olga <yavruyano90@mail.ru>
2022-12-15 15:35:51 +0300

    Has been done Homework 6

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -13,3 +13,19 @@
 [1, 3, 5]
 [1, 2, 5, 3, 10]
 """
+import random
+
+def f(x):
+    return random.randint(0, 10)
+
+list1 = [f(i) for i in range(0, 10) ]
+print(list1)
+
+list2 = [i for i in list1 if list1.count(i) == 1]
+print("список чисел, которые не повторяются в заданном списке", list2)
+
+list3 = set([i for i in list1 if list1.count(i) > 1])
+print("список повторяемых чисел ", list(list3))
+
+list4 = list2 + list(list3)
+print("список без повторений ", list4)
