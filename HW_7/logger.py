e380f4a396b05cf8cba671a99c15c5473af9f4a8
olga <yavruyano90@mail.ru>
2022-12-16 19:09:26 +0300

    Has been done Homework 7

---
+++ HW_7/logger.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+def log(func):
+    def wapper(*args):
+        with open('log.csv', 'a', endecodind='utf-8') as log_file:
+            log_file.write(f'{datetime.now()} - запущена функция {func.__nume__} {"с аргументами" if args else "без аргументов"} ({func.__doc__})\n')
+        return func(*args)
+    return wapper
+
